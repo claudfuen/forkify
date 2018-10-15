13e1f86140dd99c91271589b370285ae23d02267
Claudio <imclaudfuen@gmail.com>
2018-10-15 14:57:13 -0400

    Changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 // Global app controller
 import string from './models/Search';
-import {add, multiply} from './views/searchView';
+import * as searchView from './views/searchView';
+import axios from 'axios';
+console.log('JS Working');
 
-console.log(string, add(2,4), multiply(2,4));
+
+const claudioKey = '7304f4f19946896a98d89e79b0fe071c';
+const jonasKey = '462b1cc8d4f2730081462fbcc65136320';
+
+
+
+
+async function getResults(query) {
+}
+getResults();
+
+
+
+
+/*
+
+Submitting a Query
+All search requests should be made to the search API URL.
+
+https://www.food2fork.com/api/search
+All recipe requests should be made to the recipe details API URL.
+
+https://www.food2fork.com/api/get 
+
+*/
