7e4cf151eb4761b9edb59f654294540ca1edc7fc
Claudio <imclaudfuen@gmail.com>
2018-10-15 15:56:41 -0400

    State

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,22 @@
 // Global app controller
 import string from './models/Search';
 import * as searchView from './views/searchView';
-import axios from 'axios';
-console.log('JS Working');
+import Search from './models/Search';
 
-
-const claudioKey = '7304f4f19946896a98d89e79b0fe071c';
-const jonasKey = '462b1cc8d4f2730081462fbcc65136320';
-
-let key = claudioKey;
-
-
-async function getResults(query) {
-
-    let proxy = 'https://cors-anywhere.herokuapp.com/'
-
-    try {
-        const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        let recipes = res.data.recipes
-        console.log(recipes);
-    } catch (err) {
-        alert(err);
-    }
-}
-
-getResults('pizza');
-
-
+// Global State of the App
 /*
-
-Submitting a Query
-All search requests should be made to the search API URL.
-
-https://www.food2fork.com/api/search
-All recipe requests should be made to the recipe details API URL.
-
-https://www.food2fork.com/api/get 
-
-
-Axios works better than fetch across a multitude of things.
-
-
+* - Search Object 
+* - Current Recipe
+* - Shopping List Object
+* - Linked Recipes
 */
+
+
+
+const state = {};
+
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,29 @@
-export default 'I am an experienced developer'
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+
+    }
+
+    async getResults() {
+
+        let proxy = 'https://cors-anywhere.herokuapp.com/'
+        const key = '7304f4f19946896a98d89e79b0fe071c';
+
+        try {
+            const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            console.log(res);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (err) {
+            alert(err);
+        }
+    }
+}
+
+
+
+
+
+
